ba8675bfd60ffb2b5b0a9920a527c6f7f61e955f
jyothishwar <jyotheeshkumar2001@gmail.com>
2024-02-04 16:32:26 +0530

    Update route.js

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -10,7 +10,7 @@ export async function POST(req, res) {
   try {
     const data = await resend.emails.send({
       from: fromEmail,
-      to: ["jyotheeshkumar2001@gmail", email],//email
+      to: ["jyotheeshkumar2001@gmail", email],
       subject: subject,
       react: (
         <>
